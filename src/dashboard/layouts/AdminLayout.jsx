ce1491c93e1a47fe0ae598d3afbe3cc8fc27c3e4
amna-670 <amnaidrees670@gmail.com>
2026-08-24 21:55:38 +0500

    Remove duplicate root frontend files

--- src/dashboard/layouts/AdminLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import DashboardLayout from "./DashboardLayout";
-
-const AdminLayout = ({ children }) => (
-  <DashboardLayout role="admin">{children}</DashboardLayout>
-);
-
-export default AdminLayout;
-
--- src/dashboard/layouts/AttendeeLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import DashboardLayout from "./DashboardLayout";
-
-const AttendeeLayout = ({ children }) => (
-  <DashboardLayout role="attendee">{children}</DashboardLayout>
-);
-
-export default AttendeeLayout;
-
